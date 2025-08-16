27cc4ff0fa9fc9afe493b7f433d98a1d1ee22307
Gary Hetzel <its@gary.cool>
2025-08-15 20:09:41 -0400

    bump webview

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = `hydra`
 	app.Usage = `Standalone browser-based UI application runner`
-	app.Version = `0.4.1`
+	app.Version = `0.4.2`
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
